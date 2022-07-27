65b03cad0d39b7bdafc51391809d57467f6baf51
junrusong <junrusong0821@gmail.com>
2022-07-26 23:17:43 -0700

    move dataloading

--- main/model/net.py
+++ main/model/net.py
@@ -65,13 +65,13 @@ class TTP(nn.Module):
         self.bn64 = nn.BatchNorm3d(64)
         self.bnin = nn.BatchNorm3d(self.n_timesteps)
         self.trans1 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
-        self.trans2 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
-        self.trans3 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
-        self.trans4 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
-        self.trans5 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
-        self.trans6 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
-        self.trans7 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
-        self.trans8 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
+        # self.trans2 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
+        # self.trans3 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
+        # self.trans4 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
+        # self.trans5 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
+        # self.trans6 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
+        # self.trans7 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
+        # self.trans8 = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
 
     def get_residual_unit(self, n):
         block = BasicBlock3D_1conv
@@ -87,14 +87,15 @@ class TTP(nn.Module):
         batch_size = x.size(0)
 
         x = x.view(batch_size, self.n_timesteps, self.params.n_flow * self.params.map_height * self.params.map_width)
-        x = self.trans1(x)
-        x = self.trans2(x)
-        x = self.trans3(x)
-        x = self.trans4(x)
-        x = self.trans5(x)
-        x = self.trans6(x)
-        x = self.trans7(x)
-        x = self.trans8(x)
+        x_trans1 = self.trans1(x)
+        x = x + x_trans1
+        # x = self.trans2(x)
+        # x = self.trans3(x)
+        # x = self.trans4(x)
+        # x = self.trans5(x)
+        # x = self.trans6(x)
+        # x = self.trans7(x)
+        # x = self.trans8(x)
 
 
 
